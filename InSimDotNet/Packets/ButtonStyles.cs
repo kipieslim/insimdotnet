01b212a2e41648122386054f3ecb81aef05fd16a
Kipieslim <8994199+kipieslim@users.noreply.github.com>
2025-02-17 15:46:51 +0100

    Update ButtonStyles.cs to include all colours.

--- InSimDotNet/Packets/ButtonStyles.cs
+++ InSimDotNet/Packets/ButtonStyles.cs
@@ -7,19 +7,43 @@ namespace InSimDotNet.Packets {
     [Flags]
     public enum ButtonStyles {
         /// <summary>
-        /// Choose a standard interface color.
+        /// Colour 0: Light grey.
+        /// </summary>
+        ISB_C0 = 0,
+        /// <summary>
+        /// Colour 1: Title colour.
         /// </summary>
         ISB_C1 = 1,
 
         /// <summary>
-        /// Choose a standard interface color.
+        /// Colour 2: Unselected text
         /// </summary>
         ISB_C2 = 2,
 
         /// <summary>
-        /// Choose a standard interface color.
+        /// Colour 3: Selected text
+        /// </summary>
+        ISB_C3 = 3,
+
+        /// <summary>
+        /// Colour 4: ok
         /// </summary>
         ISB_C4 = 4,
+
+        /// <summary>
+        /// Colour 5: cancel
+        /// </summary>
+        ISB_C5 = 5,
+
+        /// <summary>
+        /// Colour 6: text string
+        /// </summary>
+        ISB_C6 = 6,
+
+        /// <summary>
+        /// Colour 7: unavailable
+        /// </summary>
+        ISB_C7 = 7,
 
         /// <summary>
         /// When clicked this button will send a <see cref="IS_BTC"/> packet.
